61901c4570439d0c0cd0654c1d9c35f661297bd3
Luke Wong <lukewong93@gmail.com>
2023-07-06 23:25:29 +0800

    feat: update community guidelines page

--- app/community-guidelines/page.tsx
+++ app/community-guidelines/page.tsx
@@ -1,7 +1,357 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 export default function CommunityGuidelines() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This is the Community Guidelines page.</h1>
+      <Fragment>
+        <div>
+          <h1>Community Guidelines</h1>
+        </div>
+        <div>
+          <div>
+            <p>
+              Hiredly is the job discovery platform that helps job seekers make
+              the right move at any point in their career. With some cooperation
+              from both job seekers and employers, we can create an easy,
+              exciting, and effective job-seeking experience for all.
+              <br></br>
+              <br></br>
+              We&apos;ve created these Community Guidelines so you can help us
+              nurture and protect this amazing community.
+            </p>
+          </div>
+          <div>
+            {/* ====== Job Seeker Guidelines ====== */}
+            <h2>Community Guidelines for Job Seekers</h2>
+            <p>
+              For those new to job searching, Hiredly provides exclusive
+              insights to help you discover your ideal workplace. With job
+              recommendations and career advice tailored to your experience,
+              impress employers easily when you chat live with them on the
+              platform. You&apos;ll be able to grab any opportunity wherever you
+              are with instant application updates and quick responses on the
+              app.
+              <br></br>
+              <br></br>
+              As you progress in your career, our experienced recruiters from
+              Hiredly X will match you to your next opportunity. No matter your
+              experience or industry, Hiredly makes job searching easy and fast.
+              <br></br>
+              <br></br>
+              Here are some basic guidelines to optimise your job search
+              experience:
+            </p>
+
+            <h3>1. Upload an accurate, truthful and professional resume.</h3>
+            <span>
+              Your resume is your golden ticket to land your dream job. It is
+              also your chance to make a good first impression on prospective
+              hiring managers. Please ensure that you have included complete and
+              accurate information on your resume, and that none of it is
+              misleading or out of date. Here are some basic things you should
+              have in your resume:
+              <br></br>
+              <ul>
+                <li>Your name</li>
+                <li>E-mail address</li>
+                <li>Current location (Eg. Petaling Jaya, Malaysia)</li>
+                <li>Contact number</li>
+                <li>Educational background</li>
+                <li>Working experience (if any)</li>
+                <li>Special skills</li>
+                <li>Past achievements and activities</li>
+              </ul>
+              If you need some guidance on how to make a good resume,&nbsp;
+              <a href="https://my.hiredly.com/advice/perfect-resume-checklist">
+                here are our tips and tricks to help impress your future boss
+              </a>
+              .
+            </span>
+
+            <h3>2. Complete your profile.</h3>
+            <span>
+              It&apos;s important to fill up your profile honestly and
+              completely. In addition to your resume, employers will also be
+              able to see your profile, as this is the standard practice with
+              all job platforms. Your profile serves as a summary that employers
+              can easily look through before diving into your resume.
+              <br></br>
+              <br></br>
+              It&apos;s also highly recommended that you record a video cover
+              letter on your profile as well. This video cover letter is sent
+              along with your application to the employers, so take the
+              opportunity to stand out and share your personality.
+              <br></br>
+              DO:
+              <br></br>
+              <ul>
+                <li>
+                  Use a proper name for your account. Employers will see the
+                  name you chose that is displayed alongside your resume.
+                </li>
+                <li>
+                  Write a simple but informative summary about you. Tell
+                  employers who you are and what kind of opportunities you are
+                  looking for.
+                </li>
+                <li>
+                  Record a quick 30-second video cover letter that is
+                  professional and informative.
+                </li>
+              </ul>
+              DO NOT:
+              <ul>
+                <li>
+                  Provide any false, misleading or inappropriate information.
+                </li>
+              </ul>
+            </span>
+
+            <h3>3. Pick a professional profile picture.</h3>
+            <span>
+              It is optional to have a profile picture on Hiredly. If you do
+              upload one, you should aim to look professional and presentable.
+              <br></br>
+              <ul>
+                <li>Ensure that your photo is clear.</li>
+                <li>Only use a picture of yourself.</li>
+                <li>Make sure that your face can be clearly seen.</li>
+                <li>Keep it professional.</li>
+              </ul>
+            </span>
+
+            <h3>4. Communicate in English.</h3>
+            <p>
+              Hiredly uses English as its main language across its platforms to
+              ensure clear communication between all parties. The use of other
+              languages is accepted and welcomed, however all written
+              information must also be presented in English alongside the chosen
+              language.
+              <br></br>
+              <br></br>
+              For example, if you want to upload your resume in another
+              language, you must at least ensure that you&apos;ve presented all the
+              same information in English too — in the same document with page 1
+              is in English and page 2 is all the same information in Bahasa
+              Melayu.
+            </p>
+
+            <h3>5. Apply for jobs responsibly.</h3>
+            <p>
+              Using the filters available, we encourage you to apply to jobs
+              that are relevant and of interest to you. Do not send applications
+              to jobs that you are not seriously interested in. Read the job
+              description before sending your application.
+              <br></br>
+              <br></br>
+              When applying for a job, you will have 30 minutes to cancel your
+              application if you change your mind. After 30 minutes, job
+              applications can no longer be cancelled or deleted.
+              <br></br>
+              <br></br>
+              Do your best to attend any interviews that have been set and
+              communicate respectfully with employers.
+            </p>
+
+            <h3>6. Take note of your messages.</h3>
+            <p>
+              With our built-in chat system, it&apos;s easier than ever for
+              employers to contact you. Keep a lookout as employers may use the
+              chat system to conduct virtual interviews, ask for more
+              information or to schedule an interview with you. Remember to be
+              courteous and take all messages seriously.
+              <br></br>
+              <br></br>
+              Do note that employers may also contact you through your email and
+              contact number provided in your resume and Hiredly profile.
+            </p>
+
+            <h3>7. Respect copyright.</h3>
+            <p>
+              Only use and upload documents that you have made or have been
+              authorised to use. This includes any information, resumes&apos;,
+              pictures, projects, etc. Copyrighted material will be removed
+              immediately upon request of the rightful owner.
+            </p>
+
+            <h3>8. We value your privacy.</h3>
+            <p>
+              Hiredly respects the privacy of individuals with regard to
+              personal data and is committed to protecting the privacy of its
+              users, and strives to provide a safe, secure user experience. If
+              you have noticed any misuse or abuse of your personal information,
+              please contact our team as soon as possible. Learn more about
+              Hiredly&apos;s privacy policy&nbsp;
+              <Link href="/">here</Link>.<br></br>
+              <br></br>
+              Please note that our Hiredly X consultants are allowed to contact
+              you for career opportunities as part of headhunting services.
+              Employers advertising on Hiredly are also allowed to contact you
+              if you have switched on the “Actively Available” toggle. However,
+              if you&apos;ve been contacted for commercial purposes or notice an
+              abuse of your information, you may inform us as this would be
+              considered misconduct according to the Employer community
+              guidelines.
+            </p>
+
+            <h3>9. One account only.</h3>
+            <p>
+              All users on Hiredly are allowed to have only one account each.
+              This is to ensure the best possible experience and proper
+              management of your information. Duplicate accounts will be removed
+              automatically upon detection.
+            </p>
+
+            <h3>10. Follow the Law.</h3>
+            <p>
+              Users are personally responsible to ensure that they fulfil and
+              comply with any existing regulations. This includes but is not
+              limited to permits, employment passes and employment regulations.
+              Hiredly is a platform that does not support any illegal practices
+              or businesses.
+            </p>
+
+            {/* ====== Employer Guidelines ====== */}
+            <h2>Community Guidelines for Employers</h2>
+            <p>
+              Hiredly is a full-fledged recruitment platform that makes it easy
+              to hire junior to mid-level talent. Our platform includes a job
+              portal and headhunting service, which you can use separately or
+              synergistically together as a hybrid solution.
+              <br></br>
+              <br></br>
+              The guidelines below will help you to optimise your hiring
+              solutions as well as create a safe job searching experience for
+              all:
+            </p>
+
+            <h3>
+              1. Create an insightful, transparent and genuine Company Profile.
+            </h3>
+            <span>
+              Your company profile gives candidates an insight into what it’s
+              like working for your company by allowing them to have a closer
+              look at your people, culture and work environment.
+              <br></br>
+              DO:
+              <br></br>
+              <ul>
+                <li>
+                  Provide as much information as you can. Fill up the ‘About Us’
+                  and ‘Culture’ write up. Transparency gives candidates a sense
+                  of confidence.
+                </li>
+                <li>
+                  Provide complete and accurate information such as in respect
+                  of your nature of business, address, website, benefits, etc.
+                </li>
+                <li>Be honest about your company’s vision and mission.</li>
+                <li>Use updated photos of your company’s workspace.</li>
+              </ul>
+              DO NOT:
+              <ul>
+                <li>
+                  Be dishonest about your office culture. Every company is
+                  different and if you are looking for the right candidate that
+                  matches your company, the key is to be accurate.
+                </li>
+                <li>
+                  Use fake or copyrighted photos that do not belong to you.
+                </li>
+                <li>
+                  Use your company profile as a platform to sell any of your
+                  products or services. Hiredly is strictly a job advertisement
+                  platform only.
+                </li>
+              </ul>
+            </span>
+
+            <h3>2. Put up informative, accurate and frank job postings.</h3>
+            <p>
+              Job descriptions should be simple but concise, these help
+              candidates understand the job and requirements easily. We highly
+              encourage employers to fill up as much information as they can
+              accurately such as job type, experience level, salary, job
+              preference, etc. This will also help serious candidates easily
+              narrow down their search to the most relevant jobs.
+            </p>
+
+            <h3>3. Communicate in English.</h3>
+            <p>
+              Hiredly uses English as its main language across its platforms to
+              ensure clear communication between all parties. The use of other
+              languages is accepted and welcomed, however all written
+              information must also be presented in English alongside the chosen
+              language. For example, a job description written in Malay must be
+              accompanied by a version written in English.
+            </p>
+
+            <h3>4. Equal Opportunity Policy.</h3>
+            <p>
+              Hiredly believes that one of the foundations of a great company
+              begins with equal access to employment opportunities for all
+              individuals, irrespective of race, colour, gender or other
+              criteria that appear to favour discrimination. All candidates
+              should be treated equally and respectfully.
+              <br></br>
+              <br></br>
+              It is important to build teams that allow individuals to realise
+              their full potential based on their aptitudes and merits, not
+              their identity. As such, we have guidelines for all content
+              featured on our platform, which are detailed within our&nbsp;
+              <Link href="/">Terms & Conditions</Link>. We
+              treat any breach of our policies or procedures seriously and will
+              remove any content that does not comply with these guidelines.
+              <br></br>
+              <br></br>
+              Hiredly strongly values the unique spirit of diversity and unity
+              in Malaysia, believing that our differences should not be
+              evaluated as part of employment decisions.
+            </p>
+
+            <h3>5. Please adhere to the privacy policy.</h3>
+            <p>
+              Hiredly respects the privacy of individuals with regards to
+              personal data and is committed to protecting the privacy of its
+              users, and strives to provide a safe, secure user experience for
+              both candidates and employers alike. This includes the management
+              of any candidate information obtained from Hiredly. Information
+              viewed or obtained from Hiredly may only be used for the purpose
+              of hiring and employment of individuals. Commercial use or abuse
+              of any information obtained from Hiredly is strictly prohibited.
+              Learn more about Hiredly&apos;s privacy policy&nbsp;
+              <Link href="/privacy-policy">here</Link>.
+            </p>
+
+            <h3>6. Follow the law.</h3>
+            <p>
+              Hiredly is an advertising platform for career opportunities and
+              thus will do its best to not interfere in any hiring processes
+              between candidates and employers. Employers are personally
+              responsible to ensure that prospective candidates fulfil and
+              comply with any existing regulations. This includes but is not
+              limited to permits, employment passes and employment regulations.
+              Hiredly is a platform that will not support any illegal practices
+              or businesses.
+            </p>
+          </div>
+          <div>
+            <p>
+              By using Hiredly, you agree to our guidelines and&nbsp;
+              <Link href="/">Terms of Use</Link>. We are
+              committed to these guidelines and we hope you will adhere to them.
+              Misconduct on Hiredly may result in accounts being disabled or
+              other restrictions.
+              <br></br>
+              Thank you for helping us create the best platform for job seekers
+              and employers to find each other.
+              <br></br>
+            </p>
+            <p>The Hiredly team.</p>
+          </div>
+        </div>
+      </Fragment>
     </main>
   );
 }
