c46ca57f6adc98a8d30cca919261f81da80e8f9b
Luke Wong <lukewong93@gmail.com>
2023-07-06 18:24:34 +0800

    feat: create community guidelines page

---
+++ app/community-guidelines/page.tsx
@@ -0,0 +1,7 @@
+export default function CommunityGuidelines() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the Community Guidelines page.</h1>
+    </main>
+  );
+}
