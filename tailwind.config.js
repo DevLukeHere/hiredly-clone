bb61278e7f43de6e1c34128cf2156770d7bee9cf
Luke Wong <lukewong93@gmail.com>
2023-07-06 23:06:18 +0800

    style: add global background colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,13 +19,7 @@ module.exports = {
       tertiary: "#ffffff",
       "background-grey": "#f5f5f5",
     },
-    extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-    },
+    extend: {},
   },
   plugins: [],
 };
