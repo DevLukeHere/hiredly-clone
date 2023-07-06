d686d1e307257ed4accad071116c57f605dab02e
Luke Wong <lukewong93@gmail.com>
2023-07-06 17:55:39 +0800

    build: configure tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,25 @@ module.exports = {
     "./_styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: "640px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+      "desktop-wide": "1536px",
+    },
+    colors: {
+      primary: "#222222",
+      secondary: "#512acc",
+      tertiary: "#ffffff",
+      "background-grey": "#f5f5f5",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [],
