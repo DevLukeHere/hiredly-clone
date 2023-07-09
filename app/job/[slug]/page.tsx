d219d0d452909a794de224d339461b9dde979f52
Luke Wong <lukewong93@gmail.com>
2023-07-09 23:33:50 +0800

    style: improve responsiveness

--- app/job/[slug]/page.tsx
+++ app/job/[slug]/page.tsx
@@ -3,7 +3,7 @@ import JobHeader from "@/app/_components/JobHeader";
 export default function Job() {
 
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen p-4 laptop:p-14">
       <JobHeader />
     </main>
   );
