0dc6522ab2830d5f5ffb9f23688a41007e8efffb
Luke Wong <lukewong93@gmail.com>
2023-07-10 11:54:22 +0800

    style: improve contrast for button

--- app/_components/ApplyJobButton.tsx
+++ app/_components/ApplyJobButton.tsx
@@ -13,7 +13,7 @@ export default function ApplyJobButton(props: ApplyJobButtonProps) {
       id={id}
       onClick={onClick}
       disabled={disabled}
-      className="h-[3rem] w-[12.5rem] laptop:w-[10rem] text-[1rem] border border-solid border-primary rounded-[6.25rem] py-[0] px-[1rem] font-[700] text-tertiary capitalize bg-button-green hover:bg-hover-green disabled:text-disabled-grey"
+      className="h-[3rem] w-[12.5rem] laptop:w-[10rem] text-[1rem] border border-solid border-primary rounded-[6.25rem] py-[0] px-[1rem] font-[700] text-primary capitalize bg-button-green hover:bg-hover-green disabled:text-disabled-grey"
     >
       {text}
     </button>
