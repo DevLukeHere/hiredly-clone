95c783f4ea9decbba2cd8e6558586a84d441d8b7
Luke Wong <lukewong93@gmail.com>
2023-07-16 22:03:46 +0800

    style: include tailwind classes to load

--- app/_components/ContainedButton.tsx
+++ app/_components/ContainedButton.tsx
@@ -4,7 +4,7 @@ interface ButtonContainedProps {
   fontSize: string; // rem
   height: string; // rem
   id?: string;
-  onClick?: any; // TODO: Declare proper type for functions
+  onClick?: () => void; 
   text: string; // lowercase
   width: string; // rem
 }
@@ -20,13 +20,16 @@ export default function ContainedButton(props: ButtonContainedProps) {
     text,
     width,
   } = props;
+  const buttonWidth = ["h-[7.5rem]", "h-[4.75rem]", "h-[12.5rem]", "h-[7rem]"]
+  const buttonHeight = ["h-[3rem]", "h-[2.75rem]", "h-[2.25rem]", "h-[1.875rem]", "h-[1.5rem]"]
+  const buttonFont = ["text-[1rem]", "text-[0.875rem]", "text-[0.75rem]"]
 
   return (
     <button
       id={id}
       onClick={onClick}
       disabled={disabled}
-      className={`h-[${height}] w-[${width}] text-[${fontSize}] border border-solid border-primary rounded-[6.25rem] py-[0] px-[1rem] font-[700] text-tertiary capitalize ${
+      className={`h-[${height}rem] w-[${width}rem] text-[${fontSize}rem] border border-solid border-primary rounded-[6.25rem] py-[0] px-[1rem] font-[700] text-tertiary capitalize ${
         buttonVariant === "primary" ? "bg-button-black" : "bg-button-green"
       } hover:${buttonVariant === "primary" ? "bg-primary" : "bg-hover-green"}
        disabled:text-disabled-grey`}
