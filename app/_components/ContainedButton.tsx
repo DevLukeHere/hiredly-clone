f181080f97634a3965c1fb21cfd9cb550b391521
Luke Wong <lukewong93@gmail.com>
2023-07-20 00:35:00 +0800

    style: improve error page

--- app/_components/ContainedButton.tsx
+++ app/_components/ContainedButton.tsx
@@ -20,7 +20,7 @@ export default function ContainedButton(props: ButtonContainedProps) {
     text,
     width,
   } = props;
-  const buttonWidth = ["h-[7.5rem]", "h-[4.75rem]", "h-[12.5rem]", "h-[7rem]"];
+  const buttonWidth = ["w-[7.5rem]", "w-[4.75rem]", "w-[12.5rem]", "w-[7rem]"];
   const buttonHeight = [
     "h-[3rem]",
     "h-[2.75rem]",
--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import ContainedButton from "./_components/ContainedButton";
+import Link from "next/link";
+
 export default function GlobalError({
   error,
   reset,
@@ -7,11 +10,54 @@ export default function GlobalError({
   error: Error;
   reset: () => void;
 }) {
+  function handlePageReload() {
+    reset();
+  }
+
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800">
+          <div className="grid grid-rows-[auto_auto_1fr] items-center gap-8">
+            <h1 className="text-tertiary text-center text-4xl capitalize font-bold">
+              {error?.name} - something went wrong!
+            </h1>
+            <p className="text-gray-300 text-sm text-center italic">
+              {error?.message}
+            </p>
+            <div className="flex gap-4 justify-center flex-wrap">
+              <ContainedButton
+                buttonVariant="secondary"
+                disabled={false}
+                text="try again"
+                fontSize="1"
+                height="2.75"
+                width="12.5"
+                onClick={handlePageReload}
+              />
+              <Link href="/" rel="noreferrer noopener">
+                <ContainedButton
+                  buttonVariant="secondary"
+                  disabled={false}
+                  text="homepage"
+                  fontSize="1"
+                  height="2.75"
+                  width="12.5"
+                />
+              </Link>
+              <Link href="/jobs" rel="noreferrer noopener">
+                <ContainedButton
+                  buttonVariant="secondary"
+                  disabled={false}
+                  text="job listings"
+                  fontSize="1"
+                  height="2.75"
+                  width="12.5"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
