8572c8d18bcecad3f01965ca3353ec07681f5ef6
Luke Wong <lukewong93@gmail.com>
2023-07-19 23:31:41 +0800

    feat: import jobs query hook

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+
+import { useJobs } from "../_hooks/useJobs";
+
 export default function Jobs() {
+  const useJobsParams = {
+    query: "",
+    first: null,
+    last: null,
+    startCursor: null,
+    endCursor: "",
+  };
+  const { jobs, pageInfo, isLoading, isError } = useJobs(useJobsParams);
+
+  // console.log("jobs:", jobs);
+  // console.log("pageInfo:", pageInfo);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the job listing page</h1>
