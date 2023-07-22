4746e7c322bc3c5fe6a4c404b1a7bf4db79bfe70
Luke Wong <lukewong93@gmail.com>
2023-07-22 11:58:50 +0800

    feat: create birthdate validation function

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -1,7 +1,8 @@
 // Reuseable functions for client side components goes here
 
 export function validateName(name: string) {
-  const pattern = /^[a-zA-Z\s@/'-]+$/; // Allow only uppercase, lowercase, spaces, and certain special characters for name
+  // Allow only uppercase, lowercase, spaces, and certain special characters for name
+  const pattern = /^[a-zA-Z\s@/'-]+$/;
 
   return pattern.test(name);
 }
@@ -10,4 +11,26 @@ export function validateEmail(email: string) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   return regex.test(email);
+}
+
+export function validateBirthdate(birthdate: string) {
+  // Convert birthdate to a Date object
+  const birthdateObj = new Date(birthdate);
+  
+  // Calculate the current date
+  const currentDate = new Date();
+  
+  // Calculate the minimum and maximum birthdate based on age range
+  const minBirthdate = new Date();
+  minBirthdate.setFullYear(currentDate.getFullYear() - 99);
+  
+  const maxBirthdate = new Date();
+  maxBirthdate.setFullYear(currentDate.getFullYear() - 16);
+  
+  // Check if the birthdate is within the valid range
+  if (birthdateObj >= minBirthdate && birthdateObj <= maxBirthdate) {
+    return true; // Birthdate is valid
+  } else {
+    return false; // Birthdate is not valid
+  }
 }
