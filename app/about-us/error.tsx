abfbd57a0b9a42cf84cb618359e0556418ae2b10
Luke Wong <lukewong93@gmail.com>
2023-07-22 11:52:50 +0800

    feat: create custom error pages

--- app/about-us/error.tsx
+++ app/about-us/error.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect } from "react";
+import ContainedButton from "../_components/ContainedButton";
+import Link from "next/link";
 
 export default function Error({
   error,
@@ -10,21 +12,55 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
+    console.error("error.cause:", error.cause);
+    console.error("error.stack:", error.stack);
   }, [error]);
 
+  function handlePageReload() {
+    reset();
+  }
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800">
+      <div className="grid grid-rows-[auto_auto_1fr] items-center gap-8">
+        <h1 className="text-tertiary text-center text-4xl capitalize font-bold">
+          {error?.name} - something went wrong!
+        </h1>
+        <p className="text-gray-300 text-sm text-center italic">
+          {error?.message}
+        </p>
+        <div className="flex gap-4 justify-center flex-wrap">
+          <ContainedButton
+            buttonVariant="secondary"
+            disabled={false}
+            text="try again"
+            fontSize="1"
+            height="2.75"
+            width="12.5"
+            onClick={handlePageReload}
+          />
+          <Link href="/" rel="noreferrer noopener">
+            <ContainedButton
+              buttonVariant="secondary"
+              disabled={false}
+              text="homepage"
+              fontSize="1"
+              height="2.75"
+              width="12.5"
+            />
+          </Link>
+          <Link href="/jobs" rel="noreferrer noopener">
+            <ContainedButton
+              buttonVariant="secondary"
+              disabled={false}
+              text="job listings"
+              fontSize="1"
+              height="2.75"
+              width="12.5"
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
