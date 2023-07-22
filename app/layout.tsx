8d929fdc5f8d63649617683015fb863041a14d2c
Luke Wong <lukewong93@gmail.com>
2023-07-22 12:17:18 +0800

    fix: resolve console warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <NavBar />
         {children}
         <Footer />
